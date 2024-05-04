42b697fa4ae94cdd14cb6ad86ed223389c47be15
PlatyPete <platypete@gmail.com>
2024-05-03 23:23:35 -0400

    Add method to ArmorSetRow to filter

--- scripts/ArmorSetRow.gd
+++ scripts/ArmorSetRow.gd
@@ -4,6 +4,8 @@ signal armor_set_pressed(armor_indices: Array)
 signal overwrite_pressed
 
 var armor_indices: Array = [0,0,0,0,0]
+var game_version: int
+var hunter_class: ArmorData.HunterClass
 
 
 func _ready():
@@ -30,3 +32,7 @@ func set_armor(new_armor_indices: Array):
 
 func set_set_name(new_name: String):
 	$SetName.set_text(new_name)
+
+
+func toggle_by_filters(active_hunter_class: ArmorData.HunterClass):
+	set_visible(game_version == ArmorData.game_version and (hunter_class == ArmorData.HunterClass.BOTH or hunter_class == active_hunter_class))
