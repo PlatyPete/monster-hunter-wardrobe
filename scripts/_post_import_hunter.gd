bb695f3d0ba2ab7722a45676093f3fa80037eb4d
PlatyPete <platypete@gmail.com>
2024-07-01 12:27:48 -0400

    Fix post import script for hunter models

--- scripts/_post_import_hunter.gd
+++ scripts/_post_import_hunter.gd
@@ -3,7 +3,7 @@ extends EditorScenePostImport
 
 # Called right after the scene is imported and gets the root node.
 func _post_import(scene):
-	var skeleton = get_node("Armature/Skeleton3D")
+	var skeleton = scene.get_node("Armature/Skeleton3D")
 
 	for mesh_instance in skeleton.get_children():
 		mesh_instance = set_mesh_materials(mesh_instance)
@@ -19,6 +19,7 @@ func set_mesh_materials(node: MeshInstance3D) -> MeshInstance3D:
 		surface_material.set_transparency(StandardMaterial3D.TRANSPARENCY_ALPHA_DEPTH_PRE_PASS)
 		surface_material.set_shading_mode(StandardMaterial3D.SHADING_MODE_PER_VERTEX)
 		surface_material.set_specular_mode(StandardMaterial3D.SPECULAR_DISABLED)
+		surface_material.set_specular(0)
 
 		node_mesh.surface_set_material(surface_index, surface_material)
 
